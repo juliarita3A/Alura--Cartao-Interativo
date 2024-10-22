a0ed93c3b570966fe23736f1d3de9e5efcff8911
Júlia Rita <00001071345114sp@al.educacao.sp.gov.br>
2024-10-21 21:44:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma linguagem de programação?',
+    'É um conjunto de instruções que um computador pode entender e executar.'
+)
+
+criaCartao(
+    'Redes de Computadores',
+    'O que é um endereço IP?',
+    'É um número que identifica um dispositivo em uma rede.'
+)
+
+criaCartao(
+    'Banco de Dados',
+    'O que é um banco de dados?',
+    'É um sistema que armazena dados de forma organizada.'
+)
+
+criaCartao(
+    'Segurança da Informação',
+    'O que é um firewall?',
+    'É uma barreira de segurança que controla o tráfego de rede.'
+)
